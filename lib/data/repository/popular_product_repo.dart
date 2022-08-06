c211d34609864796efe98d88dcb9918537362c9e
Pushedsu <kimminsu@gimminsuui-MacBookAir.local>
2022-08-06 16:26:04 +0900

    constanst util update

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -1,4 +1,5 @@
 import 'package:food_delivery/data/api/api_client.dart';
+import 'package:food_delivery/utils/app_constants.dart';
 import 'package:get/get.dart';
 //end point
 class PopularProductRepo extends GetxService{
@@ -7,7 +8,7 @@ class PopularProductRepo extends GetxService{
 
   Future<Response> getPopularProductList() async{
     //end point url 자리에 실제 url이 입력돠어야 함 ex)http://www.~~
-    return await apiClient.getData("/api/v1/products/popluar");
+    return await apiClient.getData(AppConstants.POPPULAR_PRODUCT_URI);
 
   }
 }
--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,5 +1,10 @@
-class AppContents{
+class AppConstants{
   static const String APP_NAME="DBFood";
+  static const int APP_VERSION=1;
+  //도메인 주소
+  static const String BASE_URL="https://mvs.bslmeiyu.com";
+  static const String POPPULAR_PRODUCT_URI="/api/v1/products/popluar";
+  static const String RECOMMENDED_PRODUCT_URI="/api/v1/products/recommended";
 
-
+  static const String TOKEN = "";
 }
